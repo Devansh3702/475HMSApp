6311bda4e6a2a1f88d9f5402fef7a3d1c6e5bff5
Devansh <dbs5852@psu.edu>
2023-04-12 18:03:19 -0400

    2nd

--- app/src/main/java/com/example/hms475/MainActivity.java
+++ app/src/main/java/com/example/hms475/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.hms475;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.email_input);
+        passwordEditText = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                // TODO: Implement login functionality here
+
+                Toast.makeText(MainActivity.this, "Email: " + email + "\nPassword: " + password, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
